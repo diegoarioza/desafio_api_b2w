61d99f696f4b58dd1986558da152451d065ec4ad
diego.arioza <diegoariozza@gmail.com>
2018-12-15 19:43:50 -0200

    adicionado endpoints_2

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from rest_framework.response import Response
 from .serializers import PlanetaSerializer
 from rest_framework import status
@@ -7,19 +8,24 @@ import requests
 
 
 class PlanetasViewSet(ModelViewSet):
-    amount_films = 0
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('$id', '$nome')
 
     def get_queryset(self):
         queryset = Planeta.objects.all()
         id = self.request.query_params.get('id', None)
         nome = self.request.query_params.get('nome', None)
 
-        if id or nome:
-            queryset = queryset.filter(id=id) | queryset.filter(nome=nome)
+        if id:
+            queryset = queryset.filter(id=id)
+
+        if nome:
+            queryset = queryset.filter(nome=nome)
 
         return queryset
 
     def create(self, request, *args, **kwargs):
+        exist_planet_in_api = False
         nome = request.POST.get('nome', None)
         clima = request.POST.get('clima', None)
         terreno = request.POST.get('terreno', None)
@@ -27,20 +33,23 @@ class PlanetasViewSet(ModelViewSet):
         try:
             req_api = requests.get('https://swapi.co/api/planets/?search=' + nome)
             qtde_planetas = len(req_api.json().get('results', None)[0].get('films', None))
+            if qtde_planetas > 0: exist_planet_in_api = True
         except IndexError:
             qtde_planetas = 0
 
-
-        ###############################################################################
+        print(exist_planet_in_api)
         serializer = self.get_serializer(data={'nome':nome, 'clima':clima, 'terreno': terreno, 'qtde_planetas': qtde_planetas})
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
 
+        if Planeta.objects.filter(nome=nome).exists():
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
-
+            custom_msg = {'msg': 'Planeta ja esta cadastrado'}
+            headers = self.get_success_headers(serializer.data)
+            return Response(custom_msg, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     serializer_class = PlanetaSerializer
