082bda44b6b42448e46f4d0fb758894eadde0942
diego.arioza <diegoariozza@gmail.com>
2018-12-22 11:20:34 -0200

    criando testes

--- core/tests.py
+++ core/tests.py
@@ -9,18 +9,35 @@ class StartWarsTest(APITestCase):
 
     def test_get_empty(self):
         resp = self.client.get('/starwars/')
-
         self.assertEquals(resp.status_code, 200)
 
 
-    def test_create_Dagobah(self):
+    def test_create_and_return_planet_Dagobah(self):
         # this post should return 'qtde_planetas == 3 and nome == Dagobah
         resp = self.client.post('/starwars/', data={'nome': 'Dagobah'})
+
+        # test status code
+        self.assertEquals(resp.status_code, 201)
+        # test return post
         self.assertEquals('Dagobah', resp.data.get('nome'))
         self.assertEquals(3, resp.data.get('qtde_planetas'))
-        print(resp.data)
 
-    def test_get_all(self):
+
+        # test get after post
         resp = self.client.get('/starwars/')
-        print(resp.data)
+
+        self.assertEquals(resp.status_code, 200)
+        # test status code
+        resp2 = list(resp.data[0].values())
+        # test id
+        self.assertEquals(resp2[0], 1)
+        # test Nome
+        self.assertEquals(resp2[1], 'Dagobah')
+        # test return qtde_planetas
+        self.assertEquals(resp2[4], 3)
+
+
+
+
+
 
